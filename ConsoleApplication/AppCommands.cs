ca472a9f6ea133bef3d1cfb51c8c4a85c529650b
barpinian <barpinian1213@gmail.com>
2021-06-01 16:21:17 +0300

    initial new 4 functions and tests

--- ConsoleApplication/AppCommands.cs
+++ ConsoleApplication/AppCommands.cs
@@ -16,15 +16,19 @@ namespace CshConsoleAPI
 		public const string CMD_PROMPED = "\n>";
 		public const string CMD_ECHO = "echo";
 		public const string CMD_EXIT = "exit";
+        public const string CMD_ADD = "add";
+        public const string CMD_SUB = "sub";
+        public const string CMD_MULT = "mult";
+        public const string CMD_DIV = "div";
 
 
 
 
-		/***
+        /***
 		* Echo demonstrates a command execution function.
 		* the command prints the parameters it receives.
 		*/
-		public static bool CommandEcho(string[] parameters)
+        public static bool CommandEcho(string[] parameters)
 		{
 			foreach (string param in parameters){
 				Console.WriteLine(param);
@@ -42,5 +46,70 @@ namespace CshConsoleAPI
 			Console.WriteLine(CMD_EXIT);
 			return (false);
 		}
-	}
+
+
+        public static bool CommandAdd(string[] parameters)
+        {
+            int add_value = 0;
+            foreach (string param in parameters)
+            {
+                
+                add_value += Int32.Parse(param);
+            }
+            Console.WriteLine(add_value);
+            return true;
+            
+        }
+
+
+        public static bool CommandSub(string[] parameters)
+        {
+            int sub_value = Int32.Parse(parameters[0]);
+            int first = Int32.Parse(parameters[0]);
+
+            foreach (string param in parameters)
+            {
+                
+                sub_value -= Int32.Parse(param);
+               
+            }
+            sub_value = sub_value + first;
+            Console.WriteLine(sub_value);
+            return true;
+        }
+
+
+        public static bool CommandMult(string[] parameters)
+        {
+            int mult_value = 1;
+          
+            foreach (string param in parameters)
+            {
+
+                mult_value = mult_value * Int32.Parse(param);
+
+            }
+            
+            Console.WriteLine(mult_value);
+            return true;
+
+        }
+
+
+        public static bool CommandDiv(string[] parameters)
+        {
+            double div_value = Convert.ToDouble(parameters[0]);
+            double first = Convert.ToDouble(parameters[0]);
+
+            foreach (string param in parameters)
+            {
+
+                div_value = div_value / Convert.ToDouble(param);
+
+            }
+            div_value = div_value * first;
+            Console.WriteLine(div_value);
+            return true; ;
+        }
+    }
 }
